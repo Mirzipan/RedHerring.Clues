196c0b8d73f815e03a9feb110bd2c38ca29442fa
Mirzipan <the.last.judge@gmail.com>
2023-09-16 19:41:42 +0200

    ♻️now using RedHerring.Deduction for metadata indexing

--- src/DefinitionIndexer.cs
+++ src/DefinitionIndexer.cs
@@ -1,86 +1,19 @@
 ﻿using System.Reflection;
+using RedHerring.Deduction;
 using RedHerring.Extensions.Reflection;
 
 namespace RedHerring.Clues;
 
-public class DefinitionIndexer : IDisposable
+public class DefinitionIndexer : IIndexMetadata, IDisposable
 {
     private static readonly Type DefinitionType = typeof(ADefinition);
     private static readonly Type SerializedDefinitionType = typeof(ASerializedDefinition);
     
-    private readonly HashSet<Assembly> _knownAssemblies = new();
     private readonly Dictionary<Type, Type> _serializedDefinitionToDefinitionMap = new();
-
-    public IReadOnlyCollection<Assembly> KnownAssemblies => _knownAssemblies;
 
     #region Lifecycle
 
-    public void Index()
-    {
-        foreach (var type in GetAllTypes())
-        {
-            Index(type);
-        }
-    }
-    
-    public void Dispose()
-    {
-        _knownAssemblies.Clear();
-        _serializedDefinitionToDefinitionMap.Clear();
-    }
-
-    #endregion Lifecycle
-
-    #region Manipulation
-
-    public void Add(IEnumerable<Assembly> assemblies)
-    {
-        foreach (Assembly entry in assemblies)
-        {
-            Add(entry);
-        }
-    }
-
-    public void Add(params Assembly[] assemblies)
-    {
-        Add((IEnumerable<Assembly>)assemblies);
-    }
-
-    public void Add(Assembly assembly)
-    {
-        if (_knownAssemblies.Contains(assembly))
-        {
-            return;
-        }
-
-        _knownAssemblies.Add(assembly);
-    }
-
-    #endregion Manipulation
-
-    #region Queries
-
-    public Type GetDefinitionType(Type serializedType)
-    {
-        return _serializedDefinitionToDefinitionMap.TryGetValue(serializedType, out var result) ? result : null;
-    }
-
-    #endregion Queries
-
-    #region Private
-
-    private IEnumerable<Type> GetAllTypes()
-    {
-        foreach (var assembly in _knownAssemblies)
-        {
-            foreach (var type in assembly.GetTypes())
-            {
-                yield return type;
-            }
-        }
-    }
-
-    private void Index(Type type)
+    public void Index(Type type)
     {
         if (!type.IsSubclassOf(DefinitionType) || !type.HasDefaultConstructor())
         {
@@ -103,6 +36,20 @@ public class DefinitionIndexer : IDisposable
             _serializedDefinitionToDefinitionMap[attribute.SerializedType] = type;
         }
     }
+    
+    public void Dispose()
+    {
+        _serializedDefinitionToDefinitionMap.Clear();
+    }
 
-    #endregion Private
+    #endregion Lifecycle
+
+    #region Queries
+
+    public Type GetDefinitionType(Type serializedType)
+    {
+        return _serializedDefinitionToDefinitionMap.TryGetValue(serializedType, out var result) ? result : null;
+    }
+
+    #endregion Queries
 }
